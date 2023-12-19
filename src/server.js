b14b1dde51967a427aa7fd9b2b48d08c8702788a
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-19 09:13:56 +0200

    users

--- src/server.js
+++ src/server.js
@@ -6,12 +6,39 @@ const app = express();
 
 const PORT = 3000;
 
+let users = [
+  {
+    id: 1,
+    name: 'Serbentautas',
+    town: 'Vilnius',
+    isDeleted: false,
+  },
+  {
+    id: 2,
+    name: 'Lenteja',
+    town: 'Kaunas',
+    isDeleted: false,
+  },
+  {
+    id: 3,
+    name: 'James',
+    town: 'London',
+    isDeleted: false,
+  },
+];
+
 // Middleware
 app.use(cors());
 app.use(morgan('dev'));
 
+// ROUTES
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+// GET /api/users - grazina visus vartotojus
+app.get('/api/users', (req, res) => {
+  res.json(users);
 });
 
 app.listen(PORT, () => {
