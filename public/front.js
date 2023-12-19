bce95e1bd0b953615124e7dfc616c817f668cb21
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-19 11:56:24 +0200

    uzd

--- public/front.js
+++ public/front.js
@@ -4,3 +4,9 @@ console.log('front.js file was loaded');
 const url = 'http://localhost:3000/api/books';
 
 // surinkti inputus ir issiusti i back end api
+
+// atvaizuoti visas knygas korteliu pavidalu su mygtuku edit
+
+// pasudus edit, supildoma forma su tos knygos reiksmem ir paspaus
+// submit siunciam atnaujinimo uzklausa
+// paspaudus edit pasikeicia submit mygtukas i edit
