647d22f46331306c0f3c3c7de6e315580cf13905
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-20 08:29:04 +0200

    controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,43 @@
+const users = [
+  {
+    id: 1,
+    name: 'Serbentautas',
+    town: 'Vilnius',
+    isDeleted: false,
+  },
+  {
+    id: 2,
+    name: 'Lenteja',
+    town: 'Kaunas',
+    isDeleted: false,
+  },
+  {
+    id: 3,
+    name: 'James',
+    town: 'London',
+    isDeleted: false,
+  },
+];
+
+function getAll(req, res) {
+  res.json(users);
+}
+
+const getSingle = (req, res) => {
+  const userId = +req.params.userId;
+  // surasti obj su id === userId
+  const found = users.find((uObj) => uObj.id === userId);
+  // jei neradom
+  if (found === undefined) {
+    res.status(404).json({
+      msg: `user not found with id ${userId}`,
+    });
+    return;
+  }
+  res.json(found);
+};
+
+module.exports = {
+  getAll,
+  getSingle,
+};
